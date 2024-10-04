b48526a21a65e60ca85204160eaae6f344304361
AndrewVoloshin <voloshinandy@gmai.com>
2024-10-04 11:48:07 +0300

    feat: server

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // cJvaax2bOh5lQHvK
-
+const express = require('express')
 const mongoose = require('mongoose')
+
+
+
+const app = express();
+
+
 const DB_HOST = 'mongodb://atlas-sql-66ff9c2eb1276f7035b8e741-2wsh8.a.query.mongodb.net/books_reader?ssl=true&authSource=admin'
 
 mongoose.connect(DB_HOST)
-    .then(() => console.log('Connetct to DB'))
-    .catch((error) => console.log(error.message))
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((error) => {
+        console.log(error.message)
+        process.exit(1)
+    })
 
