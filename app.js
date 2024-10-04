904b5d4ec3d150bb184a16038a2ee6f4219f3a66
AndrewVoloshin <voloshinandy@gmai.com>
2024-10-04 12:53:28 +0300

    feat: config file

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // cJvaax2bOh5lQHvK
 const express = require('express')
 const mongoose = require('mongoose');
-const { title } = require('process');
 
+const{DB_HOST} = require('./config')
 
 
 const app = express();
 
-
-const DB_HOST = 'mongodb://atlas-sql-66ff9c2eb1276f7035b8e741-2wsh8.a.query.mongodb.net/books_reader?ssl=true&authSource=admin'
 
 mongoose.connect(DB_HOST)
     .then(() => {
